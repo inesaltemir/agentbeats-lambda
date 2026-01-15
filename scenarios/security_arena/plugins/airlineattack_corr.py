3c94f6b870ff5b3a1d449faf864010a8856ce896
Ines Altemir <ines.altemir@gmail.com>
2026-01-16 00:47:13 +0100

    fix plugins

--- scenarios/security_arena/plugins/airlineattack_corr.py
+++ scenarios/security_arena/plugins/airlineattack_corr.py
@@ -10,10 +10,12 @@ Domain: Airline Customer Service
 Constraint: Prompt Injection via Query Phrasing
 """
 
+# from scenarios.security_arena.plugins.base import ScenarioPlugin
+
 import json
 import re
 from typing import Any
-from scenarios.security_arena.plugins.base import ScenarioPlugin
+from .base import ScenarioPlugin
 
 class AirlineAttackPlugin(ScenarioPlugin):
     """
